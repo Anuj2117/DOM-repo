7428dadf7536207115572831c636ae5c1680ef79
Anuj2117 <anujkushwaha221@gmail.com>
2024-04-16 22:53:11 +0530

    create a new repo for assignment

---
+++ tast1.js
@@ -0,0 +1,12 @@
+const div=document.querySelector("div");
+const para=document.querySelector("p");
+
+
+div.style.backgroundColor="yellow";
+div.style.maring="20px";
+div.style.width="200px";
+div.style.height="200px";
+
+para.style.color="blue";
+para.style.fontSize="20px";
+para.style.fontWeight="bold";
